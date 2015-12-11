7c19cf53f3cda190bbf1877ef4bd738e144e3c14
Kevin Stine <ikaikastine@gmail.com>
2015-12-10 16:18:00 -0800

    Finished implementation

--- day+night/myJS.js
+++ day+night/myJS.js
@@ -1,10 +1,35 @@
 $(document).ready(function() {
-	$('button').click(function() {
-		if($('body').hasClass('black')) {
-			$('body').removeClass('black').addClass('white');
+	$('#orb').click(function() {
+		if($('#sky').hasClass('day')) {
+			$('#sky').removeClass('day').addClass('night');
 		}
 		else {
-			$('body').removeClass('white').addClass('black');
+			$('#sky').removeClass('night').addClass('day');
+		}
+
+		if($(this).hasClass('sun')) {
+			$(this).removeClass('sun').addClass('moon');
+		}
+		else {
+			$(this).removeClass('moon').addClass('sun');
+		}
+		//Moonspot One
+		if ($('#moonspot1').hasClass('visible')) {
+			$('#moonspot1').removeClass('visible');
+		} else {
+			$('#moonspot1').addClass('visible');
+		}
+		//Moonspot Two
+		if ($('#moonspot2').hasClass('visible')) {
+			$('#moonspot2').removeClass('visible');
+		} else {
+			$('#moonspot2').addClass('visible');
+		}
+		//Moonspot Three
+		if ($('#moonspot3').hasClass('visible')) {
+			$('#moonspot3').removeClass('visible');
+		} else {
+			$('#moonspot3').addClass('visible');
 		}
 	});
 });
